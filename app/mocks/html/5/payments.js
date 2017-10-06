c1c95d980fecc9faf40bcae2c06f32a1dafaebfd
coolworld2015 <wintermute2015@ukr.net>
2017-10-06 17:44:14 +0300

    added vu-forp - payments filter

--- app/mocks/html/5/payments.js
+++ app/mocks/html/5/payments.js
@@ -25,12 +25,12 @@
 		Vue.component('payments-items', {
 			template: `	<div class="search-results-content">
 							<div class="payment" v-for="item in items">
-								<div class="search-results-item search-results-choose"><span class="circle"></span></div>
-								<div class="search-results-item search-results-sender">{{ item.status }}</div>
-								<div class="search-results-item search-results-product">Приватний</div>
-								<div class="search-results-item search-results-transfer">2155684289524</div>
+								<div class="search-results-item search-results-choose"><span class="circle">{{ searchQuery }}</span></div>
+								<div class="search-results-item search-results-sender">{{ item.cashdesk_system_id }}</div>
+								<div class="search-results-item search-results-product">{{ item.sender.cashdesk.city_id }}</div>
+								<div class="search-results-item search-results-transfer">{{ item.cashdesk_registration_id }}</div>
 								<div class="search-results-item search-results-currency">UAH</div>
-								<div class="search-results-item search-results-amount">2568.00</div>
+								<div class="search-results-item search-results-amount">{{((+item.amount).toFixed(2)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ")}}</div>
 								<div class="search-results-item search-results-date">01 Жовтня 2017</div>
 								<div class="search-results-item search-results-result long-term">
 									<span class="search-results-icon"></span>
@@ -45,21 +45,35 @@
 						</div>`,
 			data: function () {
 			  return {
-				items: []
+				items: [],
+				filteredItems: [],
+				searchQuery: ''
 			  }
 			},
 			created() {
 				this.fetchData();
+				bus.$on('searchQuery', searchQuery => {
+					this.searchQuery = searchQuery;
+					var arr = [].concat(this.filteredItems);
+					var items = arr.filter((el) => el.cashdesk_system_id.toLowerCase().indexOf(searchQuery.toLowerCase()) != -1);
+					this.items = items;
+				})
 			},
 			methods: {
 				fetchData() {
-					appConfig.message = 'Loading...'
+					appConfig.message = 'Loading...';
 					this.$http.post('http://10.18.10.8:3000/payment/listall', {                
 							id: 'id'})
-						.then(result => { 
-							//console.log(result);
+						.then(result => {
 							this.items = result.data;
+							this.filteredItems = result.data;
 						})
+				},
+				onChangeText(e) {
+					var text = appConfig.searchQuery;
+					var arr = [].concat(this.state.filteredItems);
+					var items = arr.filter((el) => el.cashdesk_system_id.toLowerCase().indexOf(text.toLowerCase()) != -1);
+					this.items = items;
 				},
 				showItem(item){
 					this.$router.push({ path: '/user-item/' + item.id + '/' + item.name + '/' + item.pass + '/' + item.description });
