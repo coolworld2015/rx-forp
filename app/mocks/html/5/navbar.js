49431831f07d6c9e2b17610d383afe2a0803267d
coolworld2015 <wintermute2015@ukr.net>
2017-10-08 11:32:52 +0300

    updated  vu-forp - navbar added logo

--- app/mocks/html/5/navbar.js
+++ app/mocks/html/5/navbar.js
@@ -1,18 +1,22 @@
 		Vue.component('navbar', {
 			template: ` <aside class="navigation-sidebar" v-bind:class="{ shown: isActive }">
 							<nav class="h-100 fp-navbar" onclick="event.preventDefault();return false">
-							
+ 
+ 
+										
+ 
 								<ul class="h-100 fp-nav-right">
-									<li class="fp-nav-item fp-nav-item-right fp-nav-close li-border-bottom" v-on:click="changeView()">
-										<a class="fp-nav-link" href="#" id="hideNav">&#x2715</a>
-									</li>
+									 
+										<img src="logo.jpg" style="width: 250px; height: 50px;" v-on:click="changeView()"> 
+										 
+									 
 									<li class="fp-nav-item fp-nav-item-right">
 										<a class="fp-nav-link menu-block-header" href="#">Платежі</a>
 									</li>
-									<li class="fp-nav-item fp-nav-item-right active" v-on:click="changeRoute('payments')">
+									<li class="fp-nav-item fp-nav-item-right" v-on:click="changeRoute('payments')" v-bind:class="{ active: payments }">
 										<a class="fp-nav-link" href="#">Миттєвий</a>
 									</li>
-									<li class="fp-nav-item fp-nav-item-right" v-on:click="changeRoute('phones')">
+									<li class="fp-nav-item fp-nav-item-right" v-on:click="changeRoute('phones')" v-bind:class="{ active: phones }">
 										<a class="fp-nav-link" href="#">Експрес</a>
 									</li>
 									<li class="fp-nav-item fp-nav-item-right" v-on:click="changeRoute('test')" v-bind:class="{ active: test }">
@@ -21,12 +25,12 @@
 									<li class="fp-nav-item fp-nav-item-right">
 										<a class="fp-nav-link" href="#">Безготівковий+</a>
 									</li>
-									<li class="fp-nav-item fp-nav-item-right hovered">
+									<li class="fp-nav-item fp-nav-item-right">
 										<a class="fp-nav-link" href="#">Cash2Card</a>
 									</li>
 									<li class="fp-nav-item fp-nav-item-right li-border-bottom">
 										<a class="fp-nav-link" href="#">Інтернет еквайринг</a>
-									</li>
+									</li><span></span> 
 									<li class="fp-nav-item fp-nav-item-right">
 										<a class="fp-nav-link menu-block-header" href="#">Реєстр операцій</a>
 									</li>
@@ -39,45 +43,45 @@
 								</ul>
 								<ul class="h-100 fp-nav-left">
 									<li class="fp-nav-item fp-nav-item-left li-border-bottom" v-on:click="changeView()">
-										<a class="fp-nav-link" href="#" id="showNav">FP</a>
+										<a class="fp-nav-link" href="#" id="showNav" title="Форпост">FP</a>
 										<span class="hot-key-hint">~</span>
 									</li>
 									<li class="fp-nav-item fp-nav-item-left">
 										<a class="fp-nav-link" href="#"></a>
 										<span class="hot-key-hint">1</span>
 									</li>
-									<li class="fp-nav-item fp-nav-item-left active" v-on:click="changeRoute('payments')">
-										<a class="fp-nav-link" href="#">М</a>
+									<li class="fp-nav-item fp-nav-item-left" v-on:click="changeRoute('payments')" v-bind:class="{ active: payments }">
+										<a class="fp-nav-link" href="#" title="Миттєвий">М</a>
 										<span class="hot-key-hint">2</span>
 									</li>
-									<li class="fp-nav-item fp-nav-item-left" v-on:click="changeRoute('phones')">
-										<a class="fp-nav-link" href="#">Е</a>
+									<li class="fp-nav-item fp-nav-item-left" v-on:click="changeRoute('phones')" v-bind:class="{ active: phones }">
+										<a class="fp-nav-link" href="#" title="Експрес">Е</a>
 									</li>
 									<li class="fp-nav-item fp-nav-item-left" v-on:click="changeRoute('test')" v-bind:class="{ active: test }">
-										<a class="fp-nav-link" href="#">Б</a>
+										<a class="fp-nav-link" href="#" title="Безготівковий">Б</a>
 										<span class="hot-key-hint">4</span>
 									</li>
 									<li class="fp-nav-item fp-nav-item-left">
-										<a class="fp-nav-link" href="#">Б+</a>
+										<a class="fp-nav-link" href="#" title="Безготівковий+">Б+</a>
 										<span class="hot-key-hint">5</span>
 									</li>
-									<li class="fp-nav-item fp-nav-item-left hovered">
-										<a class="fp-nav-link" href="#">2</a>
+									<li class="fp-nav-item fp-nav-item-left">
+										<a class="fp-nav-link" href="#" title="Cash2Card">C2</a>
 										<span class="hot-key-hint">6</span>
 									</li>
 									<li class="fp-nav-item fp-nav-item-left li-border-bottom">
-										<a class="fp-nav-link" href="#">І</a>
+										<a class="fp-nav-link" href="#" title="Інтернет еквайринг">ІE</a>
 										<span class="hot-key-hint">7</span>
 									</li>
 									<li class="fp-nav-item fp-nav-item-left">
 										<img src="">
 									</li>
 									<li class="fp-nav-item fp-nav-item-left">
-										<a class="fp-nav-link" href="#">РО</a>
+										<a class="fp-nav-link" href="#" title="Реєстр операцій">РО</a>
 										<span class="hot-key-hint">8</span>
 									</li>
 									<li class="fp-nav-item fp-nav-item-left li-border-bottom" v-on:click="changeView()">
-										<a class="fp-nav-link" href="#">КК</a>
+										<a class="fp-nav-link" href="#" title="Касова книга">КК</a>
 										<span class="hot-key-hint">9</span>
 									</li>
 								</ul>
